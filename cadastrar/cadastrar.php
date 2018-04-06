4f092cdb4ec57fe837b023574e13a0d45bf02fcc
fruggerr <38044157+fruggerr@users.noreply.github.com>
2018-04-05 21:52:46 -0300

    Finaliza o PHP e inicia o HTML do formulário.

--- cadastrar/cadastrar.php
+++ cadastrar/cadastrar.php
@@ -46,6 +46,59 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	} else {
 		$senha = trim($_POST['senha']);
 	}
+	// Validar a confirmação da senha
+	if (empty(trim(($_POST['confirma_senha'])))) {
+		$confirma_senha_err = 'Por favor, confirma a sua senha.';
+	} else {
+		$confirma_senha = trim($_POST['confirma_senha']);
+		if ($senha != $confirma_senha) {
+			$confirma_senha_err = 'As senhas não conferem.';
+		}
+	}
+	// Checa os erros da entrada antes de inserir no banco de dados
+	if (empty($matricula_err) && empty($senha_err) && empty($confirma_senha_err)) {
+		$sql = "INSERT INTO usuarios (matricula, senha) VALUES (?, ?)";
+
+		if ($stmt = mysqli_prepare($db_con, $sql)) {
+			mysqli_stmt_bind_param($stmt, "ss", $param_matricula, $param_senha);
+
+			// Setar os parâmetros
+			$param_matricula = $matricula;
+			$param_senha = password_hash($senha, PASSWORD_DEFAULT);
+
+			if (mysqli_stmt_execute($stmt)) {
+				// Redireciona para a página login
+				header("logation: login.php");
+			} else {
+				echo "Ops! Algo saiu errado. Tente novamente mais tarde.";
+			}
+		}
+		// Fecha o estado da conexão
+		mysqli_stmt_close($stmt);
+	}
+	// Fecha a conexão
+	mysqli_close($db_con);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Cadastrar usuários - Monitoração</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<div class="wrapper">
+		<h2>Cadastrar novo usuário</h2>
+		<p>Preencha o formulário para criar um novo usuário.</p>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+			<div class="form-group <?php echo (!empty($matricula_err)) ? 'has-error' : ''; ?>">
+
+		</form>
+
+	</div>
+</body>
+</html>
 }
 
 ?>
